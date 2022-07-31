bb88a9bba0cd4645a7e0ad92c4fbeab85202ea2f
ryohagenki <102887902+ryohagenki@users.noreply.github.com>
2022-08-01 08:53:20 +0900

    practice

--- src/index.js
+++ src/index.js
@@ -1,10 +1,151 @@
-import "./styles.css";
+// const,let等の変数宣言
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+//consut変数は上書き不可能
+// val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// const val5 = ['dog','cat'];
+// val5.push("monkey");
+
+// console.log(val5[2]);
+
+// const name = "ryo";
+// const age = 25;
+//「私の名前はryoです。年齢は25歳です。」
+
+//従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+//テンプレート文字列
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+//従来の関数
+// const func1 = function(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
+
+//アロー関数
+// const func2 = (str) => {
+//   return str;
+// }
+// console.log(func2("func2です"))；
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
+
+//分割代入
+// const myProfile = {
+//   name: "ryo",
+//   age: 25,
+// };
+
+// const message1 = `名前は　${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const myProfile = ['ryo', 25];
+
+// const message3 = `名前は　${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は　${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+//デフォルト値
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// sayHello();
+
+///スプレッド構文 ...
+
+//配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+//map
+// const nameArr = ["村上", "坂田", "小柳"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+//nameArr.map((name)=> console.log(name));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const nameArr = ["村上", "坂田", "小柳"];
+
+// const newNameArr = nameArr.map((name) => {
+//   if(name === "小柳") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+//三項演算子
+
+// ある条件　？　条件がtrueの時　：
+// const val1 = 1 < 0 ? 'trueです' : 'falseです'；
+// console.log(val1);
